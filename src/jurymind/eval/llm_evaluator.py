3840d886c3f64f8b3e91f7075b3b6c5ef9403a0e
George <georgedittmar@gmail.com>
2025-06-17 20:57:42 -0700

    adding more module setup

---
+++ src/jurymind/eval/llm_evaluator.py
@@ -0,0 +1,16 @@
+"""Base LLM evaluator class and modules"""
+
+class BaseEvaluator:
+    def evaluate():
+        pass
+    
+    def 
+
+class LLMEvaluator(BaseEvaluator):
+    pass
+
+class JudgeEvaluator(BaseEvaluator):
+    pass
+
+class JuryEvaluator(BaseEvaluator):
+    pass
