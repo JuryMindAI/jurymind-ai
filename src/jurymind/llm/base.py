cdc1210e5e400b137be91283cde683eb0d1f7bbc
George <georgedittmar@gmail.com>
2025-06-24 15:57:07 -0700

    filling out more base LLM class functions and comments

--- src/jurymind/llm/base.py
+++ src/jurymind/llm/base.py
@@ -1,17 +1,28 @@
 """Base class for LLM integrations"""
 from typing import Any, Self, Union
 from abc import abstractmethod
+
 class BaseLLM:
     
     def __init__(self):
         self.output_structure = None
+        self.llm = None
+        self.llm_params:dict = None
+    
+   
+    @abstractmethod
+    def __get_env_vars():
+        """Function to be implemented to grab environment variables if they are available"""
+        pass
 
     @abstractmethod
     def completion(self, prompt: Union[str]):
+        """Function to call the LLM"""
         pass
     
     @abstractmethod
     def acompletion(self, prompt: Union[str]):
+        """Async function to call the LLM if async is supported"""
         pass
 
     @abstractmethod
--- src/jurymind/llm/openai/base.py
+++ src/jurymind/llm/openai/base.py
@@ -1,4 +1,4 @@
 from llm.base import BaseLLM
-
+from abc import abstractmethod
 class OpenAI(BaseLLM):
     pass
