0de6465050917b8bd61a7c710606202adc4487d6
George <georgedittmar@gmail.com>
2025-07-05 21:29:51 -0700

    refactoring pydantic object models and moving them to core since they could be used across the framework.

---
+++ jurymind/core/models.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class ProsecutorArgument(BaseModel):
+    evidence: list
+    argument: str
+    confidence: float
+
+
+class DefenseArgument(BaseModel):
+    evidence: list
+    argument: str
+    confidence: float
+
+
+class JuryDecision(BaseModel):
+    decision: str
+    explanation: str
+    confidence: float
+
+
+class JudgeDecision(BaseModel):
+    jury_decisions: list[
+        JuryDecision
+    ]  # judge takes in decisions from jury, evidence from defense and prosecutor
+    prosecutor_arguments: list[ProsecutorArgument]
+    defense_arguments: list[DefenseArgument]
+    decisions: None
+    explanation: str
+
+
+class OptimizationStepResult(BaseModel):
+    optimized_prompt: str = Field(
+        description="Field to store the optimized prompt the agent rewrote."
+    )
+    original_prompt: str = Field(
+        description="Orginal prompt that the agent was given to optimize."
+    )
+    reason: str = Field(
+        description="Detailed explanation for the changes and why the changes were needed."
+    )
+    confidence_score: str = Field(
+        description="Likert scale between 1 to 5 on how confident the agent is."
+    )
+    stop: bool = Field(
+        description="Whether the prompt is optimal or if the agent determines it needs another iteration of rewrite."
+    )
+
+
+class OptimizationRunResult(BaseModel):
+    steps: list[OptimizationStepResult]
+    model: str
+
+
+class PromptOptimizationRunResult(OptimizationRunResult):
+    optimal: str
+    original: str
+
+
+class PromptOptimizationRequest(BaseModel):
+    prompt: str = Field(
+        description="Prompt to be optimized to best work with an LLM and Semantic Search queries."
+    )
+    examples: Optional[dict] = Field(
+        description="Optional list of examples to better tune the optimization to."
+    )
--- main.py
+++ main.py
@@ -1,27 +1,62 @@
 import os
 import asyncio
+
 # from jurymind.llm.openai import OpenAILLM
 from dotenv import load_dotenv
 from pydantic_ai import Agent
-from jurymind.agents.models import OptimizationResult, OptimizationRequest
+from jurymind.core.models import (
+    OptimizationStepResult,
+    OptimizationRunResult,
+    JudgeDecision,
+)
 
 load_dotenv()
 
 
-OPTIMIZER_INSTRUCTIONS = "Your job is to optimize a prompt from a user. You optimize by seeing how to rewrite, fix, or enhance the prompt to best work with an LLM."
+OPTIMIZER_INSTRUCTIONS = f"""Your job is to optimize a prompt from a user. You optimize 
+by seeing how to rewrite, fix, or enhance the prompt to best work with an LLM and perform the required task.
 
-agent = Agent("openai:gpt-4o", output_type=OptimizationResult, system_prompt=OPTIMIZER_INSTRUCTIONS, retries=3)
+Request to optimize:
 
-curr_prompt = "What do cat live so short do they live happy life?"
+{.model_json_schema()}
+
+Output your results like so:
+
+{OptimizationResult.model_json_schema()}
+
+result:
+"""
+print(OPTIMIZER_INSTRUCTIONS)
+agent = Agent(
+    "openai:gpt-4.1-mini",
+    output_type=OptimizationResult,
+    system_prompt=OPTIMIZER_INSTRUCTIONS,
+    retries=3,
+)
+
+judge = Agent("openai:chatgpt-4o-mini")
+
+curr_prompt = "Why do cat live do they happy life?"
 prompt_hist = []
 i = 0
-while i<10:
-    # call agent, get response and see if we should keep optimizing or not
-    result = agent.run_sync(curr_prompt)
-    print(result)
-    # if result.stop:
-    #     print("Stopping")
-    #     break
-    i += 10
-    
-print(curr_prompt) # Outputs the response
+max_iteration = 10
+
+
+def optimize(
+    optimization_request: PromptOptimizationRequest, max_iteration=5
+) -> OptimizationResult:
+    i = 0
+
+    while i < max_iteration:
+        print(f"Iteration: {i+1}")
+        # call agent, get response and see if we should keep optimizing or not
+        result = agent.run_sync(curr_prompt)
+        curr_prompt = result.output.optimized_prompt
+        prompt_hist.append(result)
+        # if result.stop:
+        #     print("Stopping")
+        #     break
+        if result.output.stop:
+            print("Stopping iteration")
+            break
+        i += 1
