dc3e5d455d52a6be809eb1a207c70575948a782b
George <georgedittmar@gmail.com>
2025-08-16 18:10:25 -0700

    fixing rebase

---
+++ jurymind/core/prompts/optimize.py
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+=======
+class BaseOptimizer:
+    pass
+
+class OptimizationPipeline:
+    """Higher level container for optimization workflows."""
+    def __init__(self, ):
+        pass
+        
+    def run(self):
+        pass
+    
+>>>>>>> 71e198e (laying out empty modules)
